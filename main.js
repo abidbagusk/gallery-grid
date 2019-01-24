946474c92aa4e16f686385cd34ef660280c32fb3
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-24 10:52:35 +0700

    create function to add div-small with image inside it

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // script for create normal div
-const container = document.querySelector('.container');
+const wrapper = document.querySelector('.wrapper');
 // 1. catch button id
 const addDiv = document.getElementById('add-small-div');
 // 2. add Event to the button
@@ -8,5 +8,12 @@ addDiv.addEventListener('click', function () {
     // const container = document.querySelector('.container');
     // 2.2 create new small div
     const smallDiv = document.createElement('div');
-
+    // 2.3 create image
+    const image = document.createElement('img');
+    image.setAttribute('src', 'https://source.unsplash.com/random');
+    image.classList.add('image-grid');
+    // 2.4 fill image to div
+    smallDiv.appendChild(image);
+    // 2.5 save to parent
+    wrapper.appendChild(smallDiv);
 });
