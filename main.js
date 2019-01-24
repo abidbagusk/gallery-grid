04097175398db8e09d560f8aea9854de917d4c79
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-24 10:35:48 +0700

    catch parent of the elemen that will be filled with content

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // script for create normal div
+const container = document.querySelector('.container');
 // 1. catch button id
 const addDiv = document.getElementById('add-small-div');
+// 2. add Event to the button
+addDiv.addEventListener('click', function () {
+    // 2.1 catch the parent of the element. the parent has been catch out of this function
+    // const container = document.querySelector('.container');
+});
